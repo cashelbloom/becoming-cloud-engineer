ce33936a1d069747b902d6e2d84efbfc4244d865
Radha <radhagansv@gmail.com>
2023-05-10 20:39:44 -0500

    I changed

--- functions/for_loop_practice.py
+++ functions/for_loop_practice.py
@@ -1,5 +1,5 @@
 price_of_items = {'apples': 0.50, 'dhaMilk': 6.00, 'proteinShake': 26.00, 'bread': 3.50, 'eggsCarton': 6.50}
-
+# new added command
 def shop_all_items(money_available, shopping_list):
     my_items = shopping_list.keys()
     total_cost_of_shopping_all_items = 0
