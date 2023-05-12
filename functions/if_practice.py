28433e2ebf1c4ade7ca93b4cfe4a807fbb929531
ramya-arikepudi <ramya.arikepudi@gmail.com>
2023-05-11 22:16:05 -0400

    if practice

---
+++ functions/if_practice.py
@@ -0,0 +1,11 @@
+x = 10
+y = 10
+
+if x < y:
+    print('x is less than y')
+elif x > y:
+    print('x is greater than y')
+elif x == y:
+    print('x and y are equal')
+else:
+    print('Cannot Compare')
