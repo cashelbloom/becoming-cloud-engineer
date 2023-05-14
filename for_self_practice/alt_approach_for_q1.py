62b0bb5e318f7d424cea364de22b38dfa84d5d2e
GanesanPerumal <cashelbloom@gmail.com>
2023-05-14 00:18:54 -0500

    added an alternative approach to original solution for question 1.

--- for_self_practice/alt_approach_for_q1.py
+++ for_self_practice/alt_approach_for_q1.py
@@ -1,8 +1,7 @@
 def printing_data_types(type_list, desc_list):
     for data_type in type_list:
-        curr_data_type = data_type
-        curr_index = type_list.index(curr_data_type)
-        print(f'{curr_index + 1}. {curr_data_type} is for {desc_list[curr_index]} values.')
+        curr_index = type_list.index(data_type)
+        print(f'{curr_index + 1}. {data_type} is for {desc_list[curr_index]} values.')
         # starting += 1
 
 
