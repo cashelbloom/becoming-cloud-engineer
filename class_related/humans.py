35f1c429ecc5772ff93350006ae31ca403f73089
GanesanPerumal <cashelbloom@gmail.com>
2023-05-31 19:44:27 -0500

    adding a new json file for class exercise.

--- class_related/humans.py
+++ class_related/humans.py
@@ -7,8 +7,8 @@ class Humans:
         self.body = body
         self.continent = 'Africa'
 
-    # def __str__(self):
-    #     return f"Name: {str(self.name)} Eyes: {self.eyes} legs: {self.legs} hands: {self.hands} body: {self.body} continent: {self.continent}"
+    def __str__(self):
+        return f"Name: {str(self.name)} Eyes: {self.eyes} legs: {self.legs} hands: {self.hands} body: {self.body} continent: {self.continent}"
     #   return f"Name: {str(self.name)} Eyes: {self.eyes} legs: {self.legs}"
     def display(self):
         print(f"Name: {self.name} Eyes: {self.eyes} legs: {self.legs} hands: {self.hands} body: {self.body} Continent: {self.continent}")
@@ -17,8 +17,12 @@ class Humans:
 my_human = Humans('Rama',"2", "2", "2", "1")
 print(my_human.display())
 #
-# my_human_with_continent = Humans('Rama',"2", "2", "2", "1")
-# print(f'trying to create an object by giving an argument meant for continent: {my_human_with_continent}')
-# my_human.display()
-# #
-# print(dir(my_human))
+my_human_with_continent = Humans('Rama',"2", "2", "2", "1")
+print(f'trying to create an object by giving an argument meant for continent: {my_human_with_continent}')
+'''
+To get all the members defined for a class:
+'''
+print(dir(my_human))
+'''
+to delete 
+'''
