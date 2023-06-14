833c2a40b241884f2504a12b4ceb701757098ce1
GanesanPerumal <cashelbloom@gmail.com>
2023-06-13 22:04:00 -0500

    added multiple files on 06/13/23

---
+++ scratch_dir/demo_of_keyword_params_and_default_params.py
@@ -0,0 +1,6 @@
+def add_numbers(z, b=400, a=300):
+    return a, b
+
+
+a, b = add_numbers()
+print(f'The value of a is: {a} and the value of b is: {b}')
---
+++ scratch_dir/review_of_topics_so_far_went_over.py
@@ -0,0 +1,22 @@
+'''
+1. Built-in Functions
+2. Writing Custom Functions
+    a. Functions without any parameters
+    b. Functions with parameters
+        1. positional parameters
+        2. keyword parameters
+            with default values
+            can be optional
+            if a value is provided to a keyword param, then the default value is overwritten
+        3.
+    c. Function ending
+        Functions can end RETURNING one or more values
+        Functions that don't return any value; however they can be of four types:
+            1. pass
+            2. exit
+            3. without the keyword return
+            4. with the keyword return
+    .
+
+
+'''
