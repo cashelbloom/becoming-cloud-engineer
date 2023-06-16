09a5a6d884cb2f317cd696ac72155fb059d46ccc
GanesanPerumal <cashelbloom@gmail.com>
2023-06-15 22:28:06 -0500

    changes made for the session on 06/15/23

--- collections/nato_alphabet_spec.py
+++ collections/nato_alphabet_spec.py
@@ -7,5 +7,7 @@ print(f'The data type of data_from_csv_file: {type(data_from_csv_file)}')
 letter_word_list = data_from_csv_file.split('\n')
 print(f'tuple: letter_word_tuple {letter_word_list}')
 new_dict = {item.split(',')[0]: item.split(',')[1] for item in letter_word_list}
+new_dict_2 = {item.split(',')[1]: item.split(',')[0] for item in letter_word_list}
 print(f'value of new_dict: {new_dict}')
+print(f'value of new_dict: {new_dict_2}')
 
