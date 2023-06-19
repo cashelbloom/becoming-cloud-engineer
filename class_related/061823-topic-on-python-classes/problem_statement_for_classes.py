13a3f3e30ee7ede07815d455684e3f0188eec82c
GanesanPerumal <cashelbloom@gmail.com>
2023-06-18 21:19:10 -0500

    added files relating to Global Chess League Solution.

---
+++ class_related/061823-topic-on-python-classes/problem_statement_for_classes.py
@@ -0,0 +1,18 @@
+'''
+Global Chess League - Chess Tournament at the Global Level
+Organizers - FIDE (World body that is responsible and managing the affairs of the game of chess across the globe
+Teams - 6 Teams
+Franchise - There ar 6 franchise
+Team Composition - In each team, there are 6 players
+                        Mixture of men and woman
+Match - Each match is played by 2 teams
+Round - How many matches will happen in each round - 3 matches
+Round Robin means each team will play against all other teams.
+Round Robin - How many matches will happen to complete one Round Robin - ans: 30 matches
+There will be 2 Round Robins
+    So, there will be a total of 60 matches played before the top 2 teams are identified:
+        for playing in the finals
+If a team with white color wins, that team will get 3 points
+If a team with blcak color wins, that team will get 4 points
+
+'''
