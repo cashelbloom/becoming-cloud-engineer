cb9477ed5b7471b7e1d4c5f658fb482f22e32126
ramya-arikepudi <ramya.arikepudi@gmail.com>
2023-07-08 19:09:43 -0400

    ramya-dev

--- ramya_workout/Car.py
+++ ramya_workout/Car.py
@@ -26,6 +26,15 @@ class Car():
         else:
             print("You can't roll back an odometer!")
 
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
+
+    def fill_gas_tank(self):
+        pass
+
+
+
 my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
 my_new_car.read_odometer()
@@ -37,4 +46,52 @@ my_new_car.update_odometer(44)
 my_new_car.read_odometer()
 
 
+class Battery():
+    """A simple attempt to model a battery for an electric car."""
+    def __init__(self, battery_size=70):
+        """Initialize the battery attributes"""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """Print a statement describing the battery size"""
+        print("This car has a " +str(self.battery_size)+ "-kwh battery." )
+
+    def get_range(self):
+        """Print a statement about the rage this battery provides"""
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+        message = "This car can go approximately " + str(range)
+        message += " miles on a full charge."
+        print(message)
+
+
+
+
+
+class ElectricCar(Car):
+    """Represent aspects of a car, specific to electric vehicles."""
+    def __init__(self,make,model,year):
+        """Initialize the attributes of a parent class"""
+        super().__init__(make,model,year)
+        """Initialize attributes specific to Electric car"""
+        # self.battery_size = 70
+        self.battery = Battery()
+
+    # def describe_battery(self):
+    #     """Print a statement describing the battery size"""
+    #     print("This car has a " +str(self.battery_size)+ "-kwh battery." )
+
+    def fill_gas_tank(self):
+        print("This car does not need a gas tank!")
+
+my_tesla = ElectricCar('tesla', 'model s', 2016)
+print(my_tesla.get_descriptive_name())
+
+#my_tesla.describe_battery()
+my_tesla.fill_gas_tank()
+
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
 
